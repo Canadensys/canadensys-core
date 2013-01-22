1edabd1fcdb7082615aef7a772c62bf15b2afd31
Peter Desmet <peter.desmet.work@gmail.com>
2013-01-22 16:31:08 +0100

    Regions + ISO 3166-2 codes
    Changed codes to 3-letter ISO 3166-2 codes and added regions. Methods
    are now named with "BE".

--- src/main/java/net/canadensys/vocabulary/stateprovince/BEProvince.java
+++ src/main/java/net/canadensys/vocabulary/stateprovince/BEProvince.java
@@ -6,26 +6,32 @@ import org.apache.commons.lang3.StringUtils;
  * Enumeration representing the Belgian provinces
  * TODO : override valueOf(String)
  * @author peterdesmet
- * Based on: http://en.wikipedia.org/wiki/Provinces_of_Belgium
+ *
+ * Sources:
+ * http://en.wikipedia.org/wiki/Provinces_of_Belgium
+ * http://en.wikipedia.org/wiki/ISO_3166-2:BE
  */
-public enum BelgiumProvince implements StateProvinceEnum{
+public enum BEProvince implements StateProvinceEnum{
 	//Provinces
-	ANTWERP("AN","Antwerp"),
-	EAST_FLANDERS("OV","East Flanders"),
-	FLEMISH_BRABANT("VB","Flemish Brabant"),
-	HAINAUT("HT","Hainaut"),
-	LIMBURG("LI","Limburg"),
-	LIEGE("LG","Liège"),
-	LUXEMBOURG("LX","Luxembourg"),
-	NAMUR("NA","Namur"),
-	WALLOON_BRABANT("BW","Walloon Brabant"),
-	WEST_FLANDERS("WV","West Flanders");
-	// This does not include the Brussels-Capital Region, as it is not a province.
-	
+	ANTWERP("VAN","Antwerp"),
+	EAST_FLANDERS("VOV","East Flanders"),
+	FLEMISH_BRABANT("VBR","Flemish Brabant"),
+	HAINAUT("WHT","Hainaut"),
+	LIMBURG("VLI","Limburg"),
+	LIEGE("WLG","Liège"),
+	LUXEMBOURG("WLX","Luxembourg"),
+	NAMUR("WNA","Namur"),
+	WALLOON_BRABANT("WBR","Walloon Brabant"),
+	WEST_FLANDERS("VWV","West Flanders"),
+	//Regions
+	BRUSSELS_CAPITAL_REGION("BRU","Brussels-Capital Region"),
+	FLEMISH_REGION("VLG","Flemish Region"),
+	WALLOON_REGION("WAL","Walloon Region");
+		
 	private final String provinceCode;
 	private final String provinceName;
 
-	private BelgiumProvince(String provinceCode, String provinceName) {
+	private BEProvince(String provinceCode, String provinceName) {
 		this.provinceCode = provinceCode;
 		this.provinceName = provinceName;
 	}
@@ -45,10 +51,10 @@ public enum BelgiumProvince implements StateProvinceEnum{
 	 * @param code
 	 * @return the matching enumeration element or null if the code could not be found.
 	 */
-	public static BelgiumProvince fromCode(String code){
+	public static BEProvince fromCode(String code){
 		if (!StringUtils.isBlank(code)){
 			String codeUpper = code.toUpperCase().trim();
-			for (BelgiumProvince p : BelgiumProvince.values()){
+			for (BEProvince p : BEProvince.values()){
 				if (codeUpper.equals(p.provinceCode)){
 					return p;
 				}
