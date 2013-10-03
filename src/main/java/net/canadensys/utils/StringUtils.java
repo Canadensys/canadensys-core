7783fd55a8f73b357d817b14a6749efe24142978
Christian Gendreau <christiangendreau@gmail.com>
2013-10-03 07:30:57 -0400

    update StringUtils

--- src/main/java/net/canadensys/utils/StringUtils.java
+++ src/main/java/net/canadensys/utils/StringUtils.java
@@ -13,6 +13,38 @@ public class StringUtils {
 	private static final Pattern pattern = Pattern.compile("\\p{InCombiningDiacriticalMarks}+");
 	
 	/**
+	 * Make all elements of a String array upper case.
+	 * @param strings string array, may contain null item
+	 * @return array containing all provided elements upper case
+	 */
+	public static String[] allUpperCase(String... strings){
+		String[] tmp = new String[strings.length];
+		for(int idx=0;idx<strings.length;idx++){
+			if(strings[idx] != null){
+				tmp[idx] = strings[idx].toUpperCase();
+			}
+		}
+		return tmp;
+	}
+	
+	/**
+	 * Make all elements of a String array lower case.
+	 * @param strings string array, may contain null item
+	 * @return array containing all provided elements lower case
+	 */
+	public static String[] allLowerCase(String... strings){
+		String[] tmp = new String[strings.length];
+		for(int idx=0;idx<strings.length;idx++){
+			if(strings[idx] != null){
+				tmp[idx] = strings[idx].toLowerCase();
+			}
+		}
+		return tmp;
+	}
+	
+	@Deprecated
+	/**
+	 * @deprecated use org.apache.commons.lang3.StringUtils.stripAccents(str)
 	 * Removes all accents on characters in a String.
 	 * éàû will return eau
 	 * @param s
--- src/test/java/net/canadensys/utils/StringUtilsTest.java
+++ src/test/java/net/canadensys/utils/StringUtilsTest.java
@@ -1,5 +1,6 @@
 package net.canadensys.utils;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.assertNull;
 
@@ -15,10 +16,35 @@ public class StringUtilsTest {
 	@Test
 	public void testUnaccent(){
 		assertTrue("j'aime les eacu.Ona".equals(StringUtils.unaccent("j'aime les éàçû.Ôñá")));
+		
+		//test alternative
+		assertTrue("j'aime les eacu.Ona".equals(org.apache.commons.lang3.StringUtils.stripAccents("j'aime les éàçû.Ôñá")));
 	}
 	
 	@Test
 	public void testUnaccentNull(){
 		assertNull(StringUtils.unaccent(null));
+		//test alternative
+		assertNull(org.apache.commons.lang3.StringUtils.stripAccents(null));
+	}
+	
+	@Test
+	public void testAllLowerCase(){
+		String[] strs = new String[]{"this","IS","a Test"};
+		String[] strsUc = StringUtils.allLowerCase(strs);
+		
+		assertEquals("this",strsUc[0]);
+		assertEquals("is",strsUc[1]);
+		assertEquals("a test",strsUc[2]);	
+	}
+	
+	@Test
+	public void testAllUpperCase(){
+		String[] strs = new String[]{"this","IS","a Test"};
+		String[] strsUc = StringUtils.allUpperCase(strs);
+		
+		assertEquals("THIS",strsUc[0]);
+		assertEquals("IS",strsUc[1]);
+		assertEquals("A TEST",strsUc[2]);
 	}
 }
