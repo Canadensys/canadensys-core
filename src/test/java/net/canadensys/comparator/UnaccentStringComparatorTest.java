9c1b4114c18682d3e03f80465144fd13ea4a61c3
Christian Gendreau <christiangendreau@gmail.com>
2014-02-17 21:23:24 -0500

    new InMemoryResourceBundle

--- src/test/java/net/canadensys/comparator/UnaccentStringComparatorTest.java
+++ src/test/java/net/canadensys/comparator/UnaccentStringComparatorTest.java
@@ -11,7 +11,7 @@ import org.junit.Test;
 
 /**
  * Test class for net.canadensys.comparator.UnaccentStringComparator
- * @author canandesys
+ * @author canadensys
  *
  */
 public class UnaccentStringComparatorTest {
